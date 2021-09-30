37e52d045adbfceaaa2c99709cc2cc76cbb66f21
jaylakhani14 <jal98@njit.edu>
2021-09-30 18:20:26 -0400

    file

---
+++ wordpress_data/wp-content/themes/followjay/header.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Tutorial theme</title>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery.js'; ?>">
+    </script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.bundle.js'; ?>">
+    </script>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/bootstrap.css'; ?>">
+    <link rel="stylesheet" href="<?php bloginfo('css/stylesheet.css'); ?>">
+</head>
+
+<body>
+
+<div id="ttr_header" class="jumbotron">
+    <h1>HEADER</h1>
+</div>
+<div class="container">
