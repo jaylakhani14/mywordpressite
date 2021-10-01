bfab48db892e9e90e835e41fe30f267531c161f2
jaylakhani14 <jal98@njit.edu>
2021-10-01 16:41:39 -0400

    fi

--- wordpress_data/wp-content/themes/followjay/functions.php
+++ wordpress_data/wp-content/themes/followjay/functions.php
@@ -1,1 +1,14 @@
 <?php
+
+function followjay_menus()
+{
+    $locations = array(
+        'primary' => "Desktop Primary Left Sidebar",
+        'footer' => "Footer Menu Items"
+    );
+
+    register_nav_menus($locations);
+
+}
+
+add_action('init','followjay_menus');
